083a431fd2bee2378d98e151fafe40ffba750d26
NITIN DHURVE <161688626+nitingithub12@users.noreply.github.com>
2024-03-04 17:38:52 +0530

    Add files via upload

---
+++ output.kt
@@ -0,0 +1,21 @@
+// the println() function is used ti o/p  show the text
+fun main(){
+
+
+   // with the help of println we will print  with backslas
+    println("hello niky bhai !!")
+    println("hello akarsh bhai !!")
+    println("hello aadarsh bhai !!")
+    println("hello mayank bhai !!")
+
+
+ //it direct calculate mathematical expresion
+    println(87+13)
+
+// with the help of print we will print but without backslas
+    print("hello aadarsh bhai !!")
+    print("hello/* mayank*/ bhai !!")
+
+
+
+}
