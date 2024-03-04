021a52adc7570d570317649a559829ec2b3974ad
NITIN DHURVE <161688626+nitingithub12@users.noreply.github.com>
2024-03-04 17:39:34 +0530

    Add files via upload

---
+++ string.kt
@@ -0,0 +1,8 @@
+//string are used for storing text
+ // astring a  colllection of charectors syrrounde by double quotes
+fun main() {
+    var greeting = "hello kitin dhurve "
+    print(greeting)
+
+
+}
